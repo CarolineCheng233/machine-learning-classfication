d31f82f7666a25f6878c670f1660e6abc317c229
CarolinaCheng616 <948005247@qq.com>
2021-05-11 22:02:52 +0800

    data

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-file_name = ""
-bert_path = "/home/chenghaoyue/chenghaoyue/code/mmaction2/work_dirs/bert_model"
+file_name = "train.txt"
+bert_path = "bert_model"
 allowed_keys = ["review_summary"]
 batch_size = 64
 epochs = 5
