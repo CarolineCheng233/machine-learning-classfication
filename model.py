966b405743be7d3d9d0dc3389d25736a429e43c9
CarolinaCheng616 <948005247@qq.com>
2021-05-16 22:06:25 +0800

    debug

--- model.py
+++ model.py
@@ -32,8 +32,6 @@ class BERT(nn.Module):
         #     text_out = self.model(**x).pooler_output
         # for i in range(len(x)):
         # import pdb; pdb.set_trace()
-        print(x.shape)
-        exit(0)
         text_out = self.model(**x).pooler_output
         return text_out
 
