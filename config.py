293b5816ec309429b8917e8f3654a01403c12eae
CarolinaCheng616 <948005247@qq.com>
2021-05-20 21:57:42 +0800

    no freeze

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ ckpt_name = "best_ckpt.pth"
 result_dir = "result"
 result_name = "test_results.txt"
 allowed_keys = ["review_summary"]
-batch_size = 128
-num_workers = 10
-epochs = 3
-save_model = False
+batch_size = 8
+num_workers = 4
+epochs = 20
+save_model = True
 
 ratio = [5.75, 1, 5.75]
 gamma = 2
 
-bert_freeze = True
-mlp_dims = (768, 256, 3)
+bert_freeze = False
+mlp_dims = (768, 3)
 mlp_layer_num = len(mlp_dims) - 1
 with_bn = True
 act_type = 'relu'
@@ -26,6 +26,6 @@ last_w_softmax = True
 
 sgd = dict(lr=0.01, momentum=0.9)
 
-log = dict(iter=50)
+log = dict(iter=20)
 log_path = "log"
 
